571d3e3e39a66707ad78891353f10555e3133693
its-kat <khalsell83@gmail.com>
2023-06-26 12:46:56 -0700

    using top-level await in modules

--- script.js
+++ script.js
@@ -22,3 +22,37 @@ add('applies', 4);
 
 //imports are not copies of the export.
 console.log(cart);
+
+console.log('Start fetching');
+//before top-level await
+// async function x() {}
+
+//////////////////////////////
+// Top-level await for modules
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('something');
+
+// real world example
+const getlastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return {
+    title: data.at(-1).title,
+    text: data.at(-1).body,
+  };
+};
+
+//an async function, which this function here clearly is, will always return a promise. It will not return the actual data itself, because by the time we are running this line of code here, the data has not yet arrived. So we still have that pending promise.
+
+const lastPost = getlastPost();
+console.log(lastPost);
+
+//use then method in regular promise to access the resolved value. Not very clean
+// lastPost.then(last => console.log(last));
+
+//using top-level await to access resolved value
+const lastPost2 = await getlastPost();
+console.log(lastPost2);
