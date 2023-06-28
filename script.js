300dd86a2b0eea7496e2ed3b34554c9b3e410aad
its-kat <khalsell83@gmail.com>
2023-06-28 07:19:04 -0700

    added dependencies to project

--- script.js
+++ script.js
@@ -108,3 +108,31 @@ console.log(ShoppingCart2.shippingCost);
 
 //Import
 // const  { addToCart } = require('./shippingCart.js')
+
+//////////////////////////////
+// Command Line
+
+//very hard to copy a nested object.
+
+import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: {
+    loggedIn: true,
+  },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+console.log(stateClone);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+//lodash
+console.log(stateDeepClone);
